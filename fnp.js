3f19d2c6581c37f02a841570216c4ff18cc9bef4
harender-singh-chauhan <harendersingh7896@gmail.com>
2022-02-19 18:54:12 +0530

    Imperative way of writing the code of finding given number is  even or odd

---
+++ fnp.js
@@ -0,0 +1,23 @@
+// fuctional programming
+// 1. Imperative & declarative writing
+// 2. pure functions & their side effects
+// 3. Mutability and Immutability
+// 4. Higher order funtion->Map, filter, reduce and their polyfills
+// ---------------------------------------
+// 
+
+// is the square of the number is even
+
+//this is the  imperative way of writing the code
+const x = 4
+const xSquared = x* x;
+
+let isEven;
+
+if(xSquared % 2 ==0){
+    isEven = true
+}
+else{
+    isEven=false
+}
+console.log(isEven);
